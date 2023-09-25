544d221916ecd6fcbb3fec562de01c490792994f
Jeanziin <jjeanfreitass@gmail.com>
2023-09-25 18:48:14 -0300

    first commit

---
+++ src/test/java/com/testmongo/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testmongo.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
